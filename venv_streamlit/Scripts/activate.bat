90e02c9802c086a239026845a7cb24d53016fc18
Gloria Givondo <gloriagivondo@gmail.com>
2024-06-04 18:19:50 +0300

    Fixed the pages

--- venv_streamlit/Scripts/activate.bat
+++ venv_streamlit/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Documents\Gloria\Analytics\GithbubProjects\churn-calculator\venv_streamlit
+set VIRTUAL_ENV=C:\Documents\Gloria\Analytics\GithbubProjects\churn-predictor-app\venv_streamlit
 
 if not defined PROMPT set PROMPT=$P$G
 
